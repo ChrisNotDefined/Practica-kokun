472972e347788ee5502419108486c1aaf1078c99
ChrisNotDefined <christopherapstarwars3@gmail.com>
2021-10-20 19:13:54 -0500

    App contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Angular';
+  numero: number = 0;
+  prev: number = 1;
+
+  fibbnacci() {
+    const nextPrev = this.numero;
+    this.numero = this.numero + this.prev;
+    this.prev = nextPrev;
+  }
 }
